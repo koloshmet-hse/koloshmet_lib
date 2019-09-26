1ccb60f781ff8d1bead27b4df73789f26ee68b91
koloshmet-hse <mvguzov@edu.hse.ru>
2019-09-26 23:34:17 +0300

    Fixed linking

--- util/string/utils.cpp
+++ util/string/utils.cpp
@@ -15,3 +15,23 @@ std::string ToUpper(std::string_view str) {
     std::transform(str.begin(), str.end(), std::back_inserter(res), transformer);
     return res;
 }
+
+auto Split(std::string_view seq, std::string_view delim) {
+    auto isDelim = [delim] (auto beg, auto end) {
+        for (auto&& cur : delim) {
+            if (beg == end) {
+                return 1;
+            }
+
+            if (cur != *beg++) {
+                return -1;
+            }
+        }
+
+        if (beg == end) {
+            return 0;
+        }
+        return -1;
+    };
+    return TSlice{std::move(seq), isDelim};
+}
